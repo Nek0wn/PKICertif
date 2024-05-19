015271035af21f9223d6308f609ca1630caf1654
Nek0wn <mathieu6868@laposte.net>
2024-05-19 15:14:46 +0200

    Modif CA sans OpenSSL

--- Code/ca.py
+++ Code/ca.py
@@ -1,7 +1,9 @@
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
-from OpenSSL import crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import base64
 import mqtt
 
 # Génération de la paire de clés pour la CA
@@ -13,15 +15,12 @@ def generate_ca_key_pair():
 
 # Génération d'un certificat auto-signé pour la CA
 def generate_ca_certificate(private_key):
-    cert = crypto.X509()
-    cert.get_subject().CN = "CA"
-    cert.set_serial_number(1000)
-    cert.gmtime_adj_notBefore(0)
-    cert.gmtime_adj_notAfter(10*365*24*60*60)
-    cert.set_issuer(cert.get_subject())
-    cert.set_pubkey(crypto.load_publickey(crypto.FILETYPE_PEM, private_key))
-    cert.sign(crypto.load_privatekey(crypto.FILETYPE_PEM, private_key), 'sha256')
-    return crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
+    key = RSA.import_key(private_key)
+    cert = {
+        "subject": "CA",
+        "public_key": key.publickey().export_key().decode()
+    }
+    return base64.b64encode(str(cert).encode()).decode()
 
 # Gestion des requêtes MQTT pour la CA
 def on_message(client, userdata, message):
