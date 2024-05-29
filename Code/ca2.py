3f92b2a453bd003eeab5755ead69f8f8a83a7e44
Nek0wn <mathieu6868@laposte.net>
2024-05-29 16:19:40 +0200

    Tentative infructueuse + Rajout fichier PEM

--- Code/ca2.py
+++ Code/ca2.py
@@ -1,65 +1,40 @@
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP, AES
-from Crypto.Util.Padding import pad, unpad
-from Crypto.Hash import SHA256
+from Crypto.PublicKey.RSA import RsaKey
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
 import base64
 import json
-import os
 import mqtt
+import os
 
-class CertificationAuthority:
+class CA:
     def __init__(self):
-        self.private_key, self.public_key = self.generate_ca_key_pair()
-        self.certificates = {}  # Store issued certificates
-        self.revoked_certificates = set()  # Store revoked certificates
-        self.symmetric_keys = {}  # Store symmetric keys for vendors
-
-    def generate_ca_key_pair(self):
-        key = RSA.generate(2048)
-        private_key = key.export_key()
-        public_key = key.publickey().export_key()
-        print(public_key)
-        return private_key, public_key
-
-    def generate_ca_certificate(self):
-        cert = {
-            "subject": "CA",
-            "public_key": self.public_key.decode()
-        }
-        return base64.b64encode(json.dumps(cert).encode()).decode()
-
-    def sign_certificate(self, cert):
-        cert_str = json.dumps(cert)
-        cert_hash = SHA256.new(cert_str.encode())
-        signer = pkcs1_15.new(RSA.import_key(self.private_key))
-        signature = signer.sign(cert_hash)
-        cert['signature'] = base64.b64encode(signature).decode()
-        return cert
-
-    def is_revoked(self, vendor_id):
-        return vendor_id in self.revoked_certificates
-
-    def revoke_certificate(self, vendor_id):
-        self.revoked_certificates.add(vendor_id)
+        self.private_key = RSA.generate(2048)
+        self.public_key = self.private_key.publickey()
+        self.crl = []  # Certificate Revocation List
+        print("CA Public Key (without newlines):")
+        print(self.public_key.export_key().decode().replace("\n", ""))
 
     def decrypt_with_private_key(self, encrypted_data):
-        rsa_cipher = PKCS1_OAEP.new(RSA.import_key(self.private_key))
+        rsa_cipher = PKCS1_OAEP.new(self.private_key)
         decrypted_data = rsa_cipher.decrypt(base64.b64decode(encrypted_data))
         return decrypted_data
 
-    def decrypt_with_symmetric_key(self, key, encrypted_data):
-        key = base64.b64decode(key)
-        encrypted_data = base64.b64decode(encrypted_data)
-        cipher = AES.new(key, AES.MODE_CBC, iv=encrypted_data[:16])
-        decrypted_data = unpad(cipher.decrypt(encrypted_data[16:]), AES.block_size)
-        return decrypted_data
+    def sign_certificate(self, public_key):
+        public_key_bytes = public_key.export_key(format='PEM')
+        hash_obj = SHA256.new(public_key_bytes)
+        signature = pkcs1_15.new(self.private_key).sign(hash_obj)
+        return base64.b64encode(signature).decode()
 
-    def encrypt_with_symmetric_key(self, key, data):
-        key = base64.b64decode(key)
-        cipher = AES.new(key, AES.MODE_CBC)
-        encrypted_data = cipher.encrypt(pad(data.encode(), AES.block_size))
-        return base64.b64encode(cipher.iv + encrypted_data).decode()
+    def revoke_certificate(self, public_key_pem):
+        self.crl.append(public_key_pem)
+        print("Certificate revoked and added to CRL.")
+
+    def is_revoked(self, public_key_pem):
+        return public_key_pem in self.crl
 
     def on_message(self, client, userdata, message):
         if message.topic == "vehicle/ca/request_cert":
@@ -67,41 +42,35 @@ class CertificationAuthority:
             vendor_id = data['vendor']
             encrypted_key = data['symmetric_key']
             symmetric_key = self.decrypt_with_private_key(encrypted_key)
-            self.symmetric_keys[vendor_id] = base64.b64encode(symmetric_key).decode()
-            mqtt.publish_message(client, "vehicle/ca/key_received", json.dumps({"vendor": vendor_id}))
 
-        elif message.topic == "vehicle/ca/vendor_public_key":
-            data = json.loads(message.payload.decode())
-            vendor_id = data['vendor']
-            encrypted_public_key = data['public_key']
-            symmetric_key = self.symmetric_keys[vendor_id]
-            vendor_public_key = self.decrypt_with_symmetric_key(symmetric_key, encrypted_public_key)
-            cert = {
-                "subject": vendor_id,
-                "public_key": vendor_public_key.decode()
-            }
-            signed_cert = self.sign_certificate(cert)
-            encrypted_cert = self.encrypt_with_symmetric_key(symmetric_key, json.dumps(signed_cert))
-            mqtt.publish_message(client, "vehicle/ca/vendor_cert", json.dumps({"vendor": vendor_id, "cert": encrypted_cert}))
+            vendor_public_key_pem = data.get("public_key")
+            if not vendor_public_key_pem:
+                vendor_public_key_pem = self.generate_key_pair().public_key().export_key().decode()
+            
+            if self.is_revoked(vendor_public_key_pem):
+                print(f"Certificate for vendor {vendor_id} is revoked.")
+                return
 
-        elif message.topic == "vehicle/ca/check_revocation":
-            vendor_id = message.payload.decode()
-            revocation_status = "Revoked" if self.is_revoked(vendor_id) else "Not Revoked"
-            mqtt.publish_message(client, "vehicle/ca/revocation_status", revocation_status)
+            vendor_public_key = RSA.import_key(vendor_public_key_pem)
+            signed_cert = self.sign_certificate(vendor_public_key)
 
-        elif message.topic == "vehicle/ca/revoke_cert":
-            vendor_id = message.payload.decode()
-            self.revoke_certificate(vendor_id)
-            mqtt.publish_message(client, "vehicle/ca/revoke_response", f"Certificate for {vendor_id} revoked")
+            encrypted_cert = self.encrypt_with_symmetric_key(symmetric_key, signed_cert)
+            response = {"cert": encrypted_cert}
+            mqtt.publish_message(client, "vehicle/ca/vendor_cert", json.dumps(response))
+            print(f"-----Certificat envoyé au vendeur {vendor_id} . . .")
 
-# Initialisation de la CA
+    def encrypt_with_symmetric_key(self, key, data):
+        cipher = AES.new(key, AES.MODE_CBC)
+        encrypted_data = cipher.encrypt(pad(data.encode(), AES.block_size))
+        return base64.b64encode(cipher.iv + encrypted_data).decode()
+
+    def generate_key_pair(self):
+        return RSA.generate(2048)
+
 def main():
-    ca = CertificationAuthority()
+    ca = CA()
     client = mqtt.initialize_mqtt_client()
     client.subscribe("vehicle/ca/request_cert")
-    client.subscribe("vehicle/ca/vendor_public_key")
-    client.subscribe("vehicle/ca/check_revocation")
-    client.subscribe("vehicle/ca/revoke_cert")
     client.on_message = ca.on_message
 
     try:
--- Code/vendeur2.py
+++ Code/vendeur2.py
@@ -6,44 +6,53 @@ import mqtt
 import os
 import base64
 
+def generate_key_pair():
+    private_key = RSA.generate(2048)
+    public_key = private_key.publickey()
+    print("Vendor Public Key (without newlines):")
+    print(public_key.export_key().decode().replace("\n", ""))
+    return private_key, public_key
+
 class Vendor:
     def __init__(self):
+        private_key, public_key = generate_key_pair()
+        self.private_key = private_key
+        self.public_key = public_key
         self.certificate = None
         self.symmetric_key = os.urandom(16)
-        self.private_key, self.public_key = self.generate_key_pair()
-
-    def generate_key_pair(self):
-        key = RSA.generate(2048)
-        private_key = key.export_key()
-        public_key = key.publickey().export_key()
-        print(public_key)
-        return private_key, public_key
 
     def encrypt_with_public_key(self, public_key, data):
-        rsa_cipher = PKCS1_OAEP.new(RSA.import_key(public_key))
+        rsa_key = RSA.import_key(public_key)
+        print("Clé publique importée avec succès :", rsa_key)
+        rsa_cipher = PKCS1_OAEP.new(rsa_key)
         encrypted_data = rsa_cipher.encrypt(data)
         return base64.b64encode(encrypted_data).decode()
 
     def encrypt_with_symmetric_key(self, key, data):
-        key = base64.b64encode(key)
         cipher = AES.new(key, AES.MODE_CBC)
         encrypted_data = cipher.encrypt(pad(data.encode(), AES.block_size))
         return base64.b64encode(cipher.iv + encrypted_data).decode()
 
     def decrypt_with_symmetric_key(self, key, encrypted_data):
-        key = base64.b64encode(key)
         encrypted_data = base64.b64decode(encrypted_data)
         cipher = AES.new(key, AES.MODE_CBC, iv=encrypted_data[:16])
         decrypted_data = unpad(cipher.decrypt(encrypted_data[16:]), AES.block_size)
         return decrypted_data
 
-    def request_certificate(self, client, ca_public_key):
+    def request_certificate(self, client, ca_public_key_pem):
+        print("CA Public Key (received):")
+        print(ca_public_key_pem.replace("\n", ""))
+        ca_public_key = RSA.import_key(ca_public_key_pem)
+        print("Etape 1 : Bonne")
         encrypted_key = self.encrypt_with_public_key(ca_public_key, self.symmetric_key)
-        mqtt.publish_message(client, "vehicle/ca/request_cert", json.dumps({"vendor": "vendor_1", "symmetric_key": encrypted_key}))
+        print("Etape 2 : Bonne")
+        mqtt.publish_message(client, "vehicle/ca/request_cert", json.dumps({"vendor": "vendor_1", "symmetric_key": encrypted_key, "public_key": self.public_key.export_key().decode()}))
         print("-----Demande de certificat envoyée à la CA . . .")
+        print("Etape 3 : Termine")
+
 
     def send_public_key(self, client):
-        encrypted_public_key = self.encrypt_with_symmetric_key(self.symmetric_key, self.public_key.decode())
+        encrypted_public_key = self.encrypt_with_symmetric_key(self.symmetric_key, self.public_key.export_key().decode())
         mqtt.publish_message(client, "vehicle/ca/vendor_public_key", json.dumps({"vendor": "vendor_1", "public_key": encrypted_public_key}))
         print("-----Clé publique envoyée à la CA . . .")
 
@@ -64,7 +73,6 @@ class Vendor:
         mqtt.publish_message(client, "vehicle/client/cert_from_vendor", json.dumps(self.certificate))
         print("-----Envoi du certificat au client . . .")
 
-# Initialisation du vendeur
 def main():
     vendor = Vendor()
     client = mqtt.initialize_mqtt_client()
@@ -73,13 +81,27 @@ def main():
     client.subscribe("vehicle/client/verify_cert")
     client.on_message = vendor.on_message
 
-    ca_public_key = "-----BEGIN PUBLIC KEY-----\n...CA public key...\n-----END PUBLIC KEY-----"
-    vendor.request_certificate(client, ca_public_key)
+    # Replace with the actual CA public key
+
+    with open('ca_public_key.pem', 'r') as file:
+        ca_public_key_pem = file.read()
+
+#     ca_public_key_pem = """-----BEGIN PUBLIC KEY-----
+# MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApTKh3f+vesPXTljr4yj7
+# 7KIxtcd5ZjdkJEIqUEPG1ktmVGcoQ1KIFq0iWst0QB1QQmyPDxZQie8xg/9DModU
+# BgnZo7rAoFt4/pGn1U8kjzls7FUm2+8bbay4dePMeEaiCy1xBztW9yeLSt0kTsJq
+# leJjhPDUzz6psSPyacY41cIbN+lhYbpPMyl6VOddsX596cXsLtV0hAAGDfjIG6RD
+# zndL9Lst+CpgLX+jM9XBhOFrhEdnjzKcNwuo2POpYKRVlDMH3kfucQzQ+ueH4J5w
+# kWJtKOT1xitfO1A6dR42vew2VbQlCYgrKwVfmCSwXPiqcUc/m7I9iMOOYSuNo7zC
+# AwIDAQAB
+# -----END PUBLIC KEY-----"""
+    
+    vendor.request_certificate(client, ca_public_key_pem)
 
     try:
         client.loop_forever()
     except KeyboardInterrupt:
-        print("Arrêt du script...")
+       print("Arrêt du script...")
     finally:
         client.disconnect()
 
